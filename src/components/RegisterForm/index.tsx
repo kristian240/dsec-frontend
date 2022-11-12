f3c13f5ad241e27734ec434327b60d3c9fc05e6b
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-12 20:21:12 +0100

    Setup register API request

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -1,20 +1,25 @@
-import { Button, FormControl, FormErrorMessage, FormLabel, Input, Text, VStack } from '@chakra-ui/react';
+import { Button, FormErrorMessage, FormLabel, Input, Text, VStack } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import { post } from '../../utils/network';
 
-export default function RegisterForm() {
-	const { handleSubmit } = useForm();
+enum RegisterFields {
+	EMAIL = 'register-email',
+	FIRST_NAME = 'register-firstname',
+	LAST_NAME = 'register-lastname',
+	PASSWORD = 'register-password',
+}
 
-	function onRegisterAction(formData) {
+export default function RegisterForm() {
+	const { handleSubmit, register } = useForm();
+
+	function onRegisterAction(formData: Record<RegisterFields, unknown>) {
 		const requestBody = {
-			data: {
-				firstName: formData['register-firstname'],
-				lastName: formData['register-lastname'],
-				email: formData['register-email'],
-				password: formData['register-password'],
-			},
+			firstName: formData[RegisterFields.FIRST_NAME],
+			lastName: formData[RegisterFields.LAST_NAME],
+			email: formData[RegisterFields.EMAIL],
+			password: formData[RegisterFields.PASSWORD],
 		};
-		post('/auth/register', requestBody)
+		post('/api/auth/register', requestBody)
 			.then((res) => {
 				console.log(res);
 			})
@@ -29,19 +34,24 @@ export default function RegisterForm() {
 		<form onSubmit={handleSubmit(onRegisterAction)}>
 			<VStack align="stretch">
 				<FormLabel>Email</FormLabel>
-				<Input type="email" placeholder="Email" name="register-email" />
+				<Input type="email" placeholder="Email" {...register(RegisterFields.EMAIL)} />
 				<FormErrorMessage>Email is required.</FormErrorMessage>
+
 				<FormLabel>First name</FormLabel>
-				<Input placeholder="First name" name="register-firstname" />
+				<Input placeholder="First name" {...register(RegisterFields.FIRST_NAME)} />
 				<FormErrorMessage>First name is required.</FormErrorMessage>
+
 				<FormLabel>Last name</FormLabel>
-				<Input placeholder="Last name" name="register-lastname" />
+				<Input placeholder="Last name" {...register(RegisterFields.LAST_NAME)} />
+
 				<FormLabel>Password</FormLabel>
-				<Input type="password" placeholder="Password" name="register-password" />
+				<Input type="password" placeholder="Password" {...register(RegisterFields.PASSWORD)} />
 				<FormErrorMessage>Password is required.</FormErrorMessage>
+
 				<Button type="submit" colorScheme="blue">
 					Sign up
 				</Button>
+
 				<Text textAlign="center">
 					By creating an account, you are accepting our terms of service and privacy and cookie policy.
 				</Text>
