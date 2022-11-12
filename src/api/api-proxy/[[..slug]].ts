02dacc6d5d33ca0634003d5b1b34014440201071
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-12 19:32:02 +0100

    Add api-proxy script

---
+++ src/api/api-proxy/[[..slug]].ts
@@ -0,0 +1,60 @@
+import { IncomingMessage } from 'http';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+let apiUrl: string;
+
+switch (process.env.PROXY_ENV) {
+	// pick API endpoint depending on the PROXY_ENV, assign to apiUrl
+	case 'production':
+		apiUrl = 'https://gbw-proxy-live.webserver-preview.fsnmm.de';
+		break;
+	case 'prelive':
+		apiUrl = 'https://gbw-proxy-uat.webserver-preview.fsnmm.de';
+		break;
+	default:
+		apiUrl = 'https://gbw-proxy-dev.webserver-preview.fsnmm.de';
+}
+
+const proxy = createProxyMiddleware({
+	target: apiUrl,
+	changeOrigin: true,
+	logLevel: 'debug',
+	cookieDomainRewrite: 'localhost',
+	pathRewrite: {
+		'^/api/api-proxy': '',
+	},
+	onProxyRes: (proxyRes: IncomingMessage) => {
+		// You can manipulate the cookie here
+
+		if (!proxyRes.headers['set-cookie']) {
+			return;
+		}
+
+		// For example you can remove secure and SameSite security flags so browser can save the cookie in dev env
+		const adaptCookiesForLocalhost = proxyRes.headers['set-cookie'].map((cookie) =>
+			cookie.replace(/; secure/gi, '').replace(/; SameSite=None/gi, '')
+		);
+
+		proxyRes.headers['set-cookie'] = adaptCookiesForLocalhost;
+	},
+	onError: (err: Error) => console.error(err),
+});
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<unknown>) {
+	// Don't allow requests to hit the proxy when not in development mode
+	// NextJS doesn't allow conditional API routes
+	if (process.env.NODE_ENV !== 'development') {
+		return res.status(404).json({ message: 'Not found' });
+	}
+
+	// @ts-ignore
+	return proxy(req, res);
+}
+
+export const config = {
+	api: {
+		bodyParser: false, // enable POST requests
+		externalResolver: true, // hide warning message
+	},
+};
