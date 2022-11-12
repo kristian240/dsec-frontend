714b7cbc081e753ecee15cad33d2f1c2d7ec6df1
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-12 20:47:43 +0100

    Remove production guard

--- src/pages/api/api-proxy/[[...slug]].ts
+++ src/pages/api/api-proxy/[[...slug]].ts
@@ -41,9 +41,9 @@ const proxy = createProxyMiddleware({
 export default function handler(req: NextApiRequest, res: NextApiResponse<unknown>) {
 	// Don't allow requests to hit the proxy when not in development mode
 	// NextJS doesn't allow conditional API routes
-	if (process.env.NODE_ENV !== 'development') {
-		return res.status(404).json({ message: 'Not found' });
-	}
+	// if (process.env.NODE_ENV !== 'development') {
+	// 	return res.status(404).json({ message: 'Not found' });
+	// }
 
 	// @ts-ignore
 	return proxy(req, res);
