5aa62c7cabb3520a0a1d8a4494865fc2a684d1a9
Lucija <lucija.strejcek@fer.hr>
2022-11-12 18:51:42 +0100

    Add auth header

---
+++ src/components/AuthHeader/index.tsx
@@ -0,0 +1,30 @@
+import { Box, Heading, LinkBox, LinkOverlay, Tab, TabList, Tabs, Text, useControllableState } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
+const indexMap = { '/login': 0, '/register': 1 };
+export default function AuthHeader() {
+	const router = useRouter();
+
+	return (
+		<Box textAlign="center">
+			<Heading as="b">DSEC</Heading>
+			<Text>
+				The simplest way to create <b>secure software</b>
+			</Text>
+			<Tabs isFitted index={indexMap[router.pathname]}>
+				<TabList>
+					<Tab>
+						<LinkBox>
+							<LinkOverlay href="/login">Login</LinkOverlay>
+						</LinkBox>
+					</Tab>
+					<Tab>
+						<LinkBox>
+							<LinkOverlay href="/register">Register</LinkOverlay>
+						</LinkBox>
+					</Tab>
+				</TabList>
+			</Tabs>
+		</Box>
+	);
+}
