d5cda97d30928eaa6360bfa27b5faa1980cae6ed
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-13 21:31:46 +0100

    Add Logo

---
+++ src/icon/Logo.tsx
@@ -0,0 +1,12 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+import React, { FC } from 'react';
+
+interface ILogoSvgProps {}
+
+export const LogoSvg: FC<IconProps> = (props) => {
+	return (
+		<Icon color="primary.500" viewBox="0 0 38 39" {...props}>
+			<path d="M0 .5h19a19 19 0 1 1-19 19V.5Z" fill="currentColor" />
+		</Icon>
+	);
+};
