f789d6a60da36c5ec39dcbfaf957ffd71a2bd89c
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-13 21:30:51 +0100

    Extend the default theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import themeOverrides from '@/styles/theme';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { appWithTranslation } from 'next-i18next';
 
+const extendedTheme = extendTheme(themeOverrides);
+
 const DSecApp = ({ Component, pageProps }) => (
-	<ChakraProvider>
+	<ChakraProvider theme={extendedTheme}>
 		<Component {...pageProps} />
 	</ChakraProvider>
 );
