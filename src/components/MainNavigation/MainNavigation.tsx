6b2a497188206b3eae880c5655b5c3ae8f8e6e9d
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-13 21:56:50 +0100

    Fix nav links

--- src/components/MainNavigation/MainNavigation.tsx
+++ src/components/MainNavigation/MainNavigation.tsx
@@ -14,19 +14,19 @@ export const MainNavigation: FC<IMainNavigationProps> = ({ ...rest }) => {
 				<Link as={NextLink} href="/features">
 					{t('label.feature_other')}
 				</Link>
-				<Link as={NextLink} href="/features">
+				<Link as={NextLink} href="/about-ssdlc">
 					{t('label.aboutSsdlc')}
 				</Link>
 			</Flex>
 
 			<Flex align="inherit" gap={8}>
-				<Link as={NextLink} href="/features">
+				<Link as={NextLink} href="/help">
 					{t('label.help')}
 				</Link>
-				<Link as={NextLink} href="/features">
+				<Link as={NextLink} href="/login">
 					{t('label.logIn')}
 				</Link>
-				<Button as={NextLink} href="/features" variant="solid" borderRadius="full" colorScheme="primary">
+				<Button as={NextLink} href="/register" variant="solid" borderRadius="full" colorScheme="primary">
 					{t('label.signUp')}
 				</Button>
 			</Flex>
