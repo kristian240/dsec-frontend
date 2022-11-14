bffcfce54fcf6c73b77c7f107ad020d8411d0217
Lucija <lucija.strejcek@fer.hr>
2022-11-14 10:16:25 +0100

    Add main layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,15 @@
 import AuthHeader from '@/components/AuthHeader';
 import LoginForm from '@/components/LoginForm';
+import { MainLayout } from '@/components/MainLayout/MainLayout';
 import { Box } from '@chakra-ui/react';
 
 export default function LoginPage() {
 	return (
-		<Box mx="auto" my="100px" maxW="500px">
-			<AuthHeader />
-			<LoginForm />
-		</Box>
+		<MainLayout>
+			<Box mx="auto" my="100px" maxW="500px">
+				<AuthHeader />
+				<LoginForm />
+			</Box>
+		</MainLayout>
 	);
 }
