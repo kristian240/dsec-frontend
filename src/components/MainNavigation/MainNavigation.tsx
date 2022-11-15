ecc612a7af250178747300e3ba4aa5c64b2796b6
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-15 08:17:27 +0100

    Dynamic navigation

--- src/components/MainNavigation/MainNavigation.tsx
+++ src/components/MainNavigation/MainNavigation.tsx
@@ -1,15 +1,32 @@
-import { Button, Flex, Link } from '@chakra-ui/react';
+import { logout } from '@/components/MainNavigation/utils';
+import { useUser } from '@/hooks/useUser';
+import { ChevronCircleIcon } from '@/icon/ChevronCircleIcon';
+import { Button, Flex, FlexProps, IconButton, Link, MenuDivider } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
+import dynamic from 'next/dynamic';
 import NextLink from 'next/link';
 import { FC } from 'react';
+import { mutate, useSWRConfig } from 'swr';
+import useMutation from 'use-mutation';
 
-interface IMainNavigationProps {}
+// dynamically import components used when user is logged in
+const Menu = dynamic(() => import('@chakra-ui/react').then((mod) => mod.Menu));
+const MenuButton = dynamic(() => import('@chakra-ui/react').then((mod) => mod.MenuButton));
+const MenuItem = dynamic(() => import('@chakra-ui/react').then((mod) => mod.MenuItem));
+const MenuList = dynamic(() => import('@chakra-ui/react').then((mod) => mod.MenuList));
 
-export const MainNavigation: FC<IMainNavigationProps> = ({ ...rest }) => {
+export const MainNavigation: FC<FlexProps> = (props) => {
 	const { t } = useTranslation('common');
+	const { data, mutate } = useUser();
+	const { cache } = useSWRConfig();
+
+	const [handleLogout] = useMutation(logout, {
+		// @ts-ignore `clear` exists on `cache` but is not in the type definition
+		onSuccess: () => cache.clear(),
+	});
 
 	return (
-		<Flex as="nav" justify="space-between" align="inherit" flex={1} {...rest}>
+		<Flex as="nav" justify="space-between" align="inherit" flex={1} {...props}>
 			<Flex align="inherit" gap={8}>
 				<Link as={NextLink} href="/features">
 					{t('label.feature_other')}
@@ -23,12 +40,32 @@ export const MainNavigation: FC<IMainNavigationProps> = ({ ...rest }) => {
 				<Link as={NextLink} href="/help">
 					{t('label.help')}
 				</Link>
-				<Link as={NextLink} href="/login">
-					{t('label.logIn')}
-				</Link>
-				<Button as={NextLink} href="/register" variant="solid" borderRadius="full" colorScheme="primary">
-					{t('label.signUp')}
-				</Button>
+
+				{data ? (
+					<Menu>
+						<MenuButton as={IconButton} variant="unstyled" icon={<ChevronCircleIcon boxSize={10} />} />
+
+						<MenuList>
+							<MenuItem as={NextLink} href="/profile">
+								Profile
+							</MenuItem>
+							<MenuItem as={NextLink} href="/dashboard">
+								Dashboard
+							</MenuItem>
+							<MenuDivider />
+							<MenuItem onClick={handleLogout}>Log out</MenuItem>
+						</MenuList>
+					</Menu>
+				) : (
+					<>
+						<Link as={NextLink} href="/login">
+							{t('label.logIn')}
+						</Link>
+						<Button as={NextLink} href="/register" variant="solid" borderRadius="full" colorScheme="primary">
+							{t('label.signUp')}
+						</Button>
+					</>
+				)}
 			</Flex>
 		</Flex>
 	);
