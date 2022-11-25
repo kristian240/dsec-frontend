b1be92f668ad5a2092b9839e6aab76b10976c06f
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-25 10:47:54 +0100

    Fix api-proxy

--- types/common.d.ts
+++ types/common.d.ts
@@ -2,7 +2,7 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			/** env for local api proxy */
-			API_PROXY_ENV: 'production' | 'development';
+			API_PROXY_ENV: 'production' | 'staging' | 'development';
 
 			/** url used to change the base url for making API calls to the server */
 			NEXT_PUBLIC_API_BASE_PATH: string;
