b8df8e2db7284c197e7bb1bf0b4e254556fdcde8
Lucija <lucija.strejcek@fer.hr>
2022-11-25 08:56:08 +0100

    Change mode in hook

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -11,7 +11,7 @@ export default function RegisterForm() {
 		formState: { errors },
 	} = useForm<RegisterFormValues>({
 		criteriaMode: 'all',
-		mode: 'all',
+		mode: 'onBlur',
 	});
 
 	const toast = useToast();
