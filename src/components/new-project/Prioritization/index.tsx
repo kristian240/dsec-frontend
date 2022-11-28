97953411b99683afd0bccda312e64139fab3dcd7
Lucija <lucija.strejcek@fer.hr>
2022-11-28 19:38:39 +0100

    Fix prioritization page

--- src/components/new-project/Prioritization/index.tsx
+++ src/components/new-project/Prioritization/index.tsx
@@ -1,12 +1,12 @@
 import {
 	Container,
-	Heading,
 	Slider,
 	SliderFilledTrack,
 	SliderThumb,
 	SliderTrack,
 	Stack,
 	StackProps,
+	Text,
 } from '@chakra-ui/react';
 import { FC } from 'react';
 
@@ -15,36 +15,30 @@ interface IPrioritizationProps {}
 export const Prioritization: FC<StackProps> = (props) => {
 	return (
 		<Container as={Stack} spacing={8} {...props}>
-			<p>
+			<Text>
 				In this step we ask you to prioritize what security aspects are important for your project. All aspects are of
 				course important, but we will use this to show you the most important information first.
-			</p>
-			<Heading as="h4" size="md">
-				Data security
-			</Heading>
-			<Slider colorScheme="blackAlpha" defaultValue={30}>
-				<SliderTrack>
-					<SliderFilledTrack />
+			</Text>
+			<Text as="b">Data security</Text>
+			<Slider colorScheme="blackAlpha" defaultValue={3} max={10}>
+				<SliderTrack boxSize={2.5} borderRadius={10} backgroundColor="#F5F5F5">
+					<SliderFilledTrack backgroundColor="black" />
 				</SliderTrack>
-				<SliderThumb />
+				<SliderThumb backgroundColor="black" />
 			</Slider>
-			<Heading as="h4" size="md">
-				Service availability
-			</Heading>
-			<Slider colorScheme="blackAlpha" defaultValue={30}>
-				<SliderTrack>
-					<SliderFilledTrack />
+			<Text as="b">Service availability</Text>
+			<Slider colorScheme="blackAlpha" defaultValue={3} max={10}>
+				<SliderTrack boxSize={2.5} borderRadius={10} backgroundColor="#F5F5F5">
+					<SliderFilledTrack backgroundColor="black" />
 				</SliderTrack>
-				<SliderThumb />
+				<SliderThumb backgroundColor="black" />
 			</Slider>
-			<Heading as="h4" size="md">
-				Some other aspects
-			</Heading>
-			<Slider colorScheme="blackAlpha" defaultValue={30}>
-				<SliderTrack>
-					<SliderFilledTrack />
+			<Text as="b">Some other aspects</Text>
+			<Slider colorScheme="blackAlpha" defaultValue={3} max={10}>
+				<SliderTrack boxSize={2.5} borderRadius={10} backgroundColor="#F5F5F5">
+					<SliderFilledTrack backgroundColor="black" />
 				</SliderTrack>
-				<SliderThumb />
+				<SliderThumb backgroundColor="black" />
 			</Slider>
 		</Container>
 	);
