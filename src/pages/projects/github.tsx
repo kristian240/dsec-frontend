a5328f4999476167509f3e14e902fac107db2c8b
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-11-28 22:13:27 +0100

    Fix

--- src/pages/projects/github.tsx
+++ src/pages/projects/github.tsx
@@ -6,15 +6,35 @@ import { Button } from '@chakra-ui/react';
 import { existsSync } from 'fs';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import path from 'path';
+import { useEffect, useState } from 'react';
 
 const githubLink = [
 	process.env.NEXT_PUBLIC_API_BASE_PATH || '',
 	'/api/api-proxy/api/oauth2/authorization/github?redirect_uri=https://',
-	process.env.VERCEL_URL,
+	typeof window !== 'undefined' ? window.location.origin : null,
 	process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URL,
 ].join('');
 
 export default function GithubPage() {
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
+	if (!isClient) {
+		return (
+			<MainLayout
+				navigation={<MainNavigation />}
+				containerProps={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+			>
+				<Button colorScheme="primary" leftIcon={<GithubIcon />}>
+					Integrate with Github
+				</Button>
+			</MainLayout>
+		);
+	}
+
 	return (
 		<>
 			<AuthRedirect to="/login" />
