02fec018316fcb89ba9ac6fbf46d2ddef4a44ef1
Lucija <lucija.strejcek@fer.hr>
2022-12-08 11:00:37 +0100

    Add react hook form

--- src/components/new-repository/NewRepository/index.tsx
+++ src/components/new-repository/NewRepository/index.tsx
@@ -3,11 +3,12 @@ import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { useFormContext } from 'react-hook-form';
 import useSWR from 'swr';
+import { RepoFields, RepoFormValues } from '../CreateRepositoryFormLayout/utils';
 
 interface INewRepositoryProps {}
 
 export const NewRepository: FC<StackProps> = (props) => {
-	const { register } = useFormContext();
+	const { register } = useFormContext<RepoFormValues>();
 	const router = useRouter();
 	const { data: repos } = useSWR('/api/github/user/repos', {
 		onError: (error) => {
@@ -21,23 +22,27 @@ export const NewRepository: FC<StackProps> = (props) => {
 	}
 
 	return (
-		<Container as={Stack} spacing={8} {...props}>
-			<FormControl>
-				<FormLabel>Select repository</FormLabel>
-				<Select>
-					{repos.map((repo) => (
-						<option key={repo.id}>{repo.full_name}</option>
-					))}
-				</Select>
-			</FormControl>
-			<FormControl>
-				<FormLabel>Name</FormLabel>
-				<Input placeholder="Name" {...register('a')} />
-			</FormControl>
-			<FormControl>
-				<FormLabel>Description</FormLabel>
-				<Textarea placeholder="Description" {...register('d')} />
-			</FormControl>
-		</Container>
+		<>
+			<Container as={Stack} spacing={8} {...props}>
+				<FormControl>
+					<FormLabel>Select repository</FormLabel>
+					<Select>
+						{repos.map((repo) => (
+							<option key={repo.id} value={repo.name} {...register(RepoFields.REPO)}>
+								{repo.full_name}
+							</option>
+						))}
+					</Select>
+				</FormControl>
+				<FormControl>
+					<FormLabel>Name</FormLabel>
+					<Input placeholder="Name" {...register(RepoFields.NAME)} />
+				</FormControl>
+				<FormControl>
+					<FormLabel>Description</FormLabel>
+					<Textarea placeholder="Description" {...register(RepoFields.DESCRIPTION)} />
+				</FormControl>
+			</Container>
+		</>
 	);
 };
