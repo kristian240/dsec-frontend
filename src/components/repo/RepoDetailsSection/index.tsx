c6d84f689a62b25c505edb3d5c8bf5e3ca7007b5
Lucija <lucija.strejcek@fer.hr>
2023-01-03 10:28:26 +0100

    Fix priority max value

--- src/components/repo/RepoDetailsSection/index.tsx
+++ src/components/repo/RepoDetailsSection/index.tsx
@@ -54,8 +54,8 @@ export const RepoDetailsSection: FC<IRepoDetailsSectionProps> = ({ repoId, ...re
 				<Tag colorScheme="cyan">Type: {data.type}</Tag>
 				<Tag colorScheme="green">Domain: {data.domain}</Tag>
 				<Tag colorScheme="blue">User data: {data.userData ? '☑️' : '❌'}</Tag>
-				<Tag colorScheme="pink">Availability: {data.availability} / 6</Tag>
-				<Tag colorScheme="red">Security: {data.security} / 6</Tag>
+				<Tag colorScheme="pink">Availability: {data.availability} / 5</Tag>
+				<Tag colorScheme="red">Security: {data.security} / 5</Tag>
 				<Tag colorScheme="gray">
 					<TagLabel>
 						<Link href={data.htmlUrl} isExternal>
