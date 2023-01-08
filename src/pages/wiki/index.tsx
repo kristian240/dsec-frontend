d0c0e9ce483311194d9cefc6422510e98d0138ad
André Schjøth <aschjoth@gmail.com>
2023-01-08 22:10:36 +0100

    feat: add page description

--- src/pages/wiki/index.tsx
+++ src/pages/wiki/index.tsx
@@ -1,7 +1,7 @@
 import { MainLayout } from '@/components/MainLayout/MainLayout';
 import { MainNavigation } from '@/components/MainNavigation/MainNavigation';
 import Topic, { Topics } from '@/components/wiki/Topic';
-import { Container, Flex, Heading } from '@chakra-ui/react';
+import { Container, Flex, Heading, Text } from '@chakra-ui/react';
 import { InferGetStaticPropsType, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
@@ -11,9 +11,13 @@ const Wiki: NextPage<WikiPageProps> = () => {
 	return (
 		<MainLayout navigation={<MainNavigation />}>
 			<Container maxW="container.xl" mt={16}>
-				<Heading as="h2" textAlign="center">
-					Wiki
+				<Heading as="h1" textAlign="center" className="albra" mb="0.5em">
+					SSLDC
 				</Heading>
+				<Text mx="auto" w="500px" fontSize="1rem" mb="5rem">
+					The point of DSEC is to help you implement a SSDLC. Therefore, we have carefully selected some tips for every
+					stage of the process.
+				</Text>
 
 				<Flex wrap="wrap" gap="2rem" justifyContent="center" mt="3rem">
 					<Topic variant={Topics.Requirements} />
